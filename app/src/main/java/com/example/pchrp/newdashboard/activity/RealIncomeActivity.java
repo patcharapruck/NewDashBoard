659d6dfde2cc78f41a8346dda1e9a942c911645f
Naphatsanan <naphatsanan.boo@msu.ac.th>
2019-01-22 23:30:43 +0700

    เพิ่ม โค้ด ของ Activity real income

---
+++ app/src/main/java/com/example/pchrp/newdashboard/activity/RealIncomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.pchrp.newdashboard.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pchrp.newdashboard.R;
+
+public class RealIncomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_income);
+    }
+}

5e97057f395ff3a7a85324a3852d9a2a6351a9ee
PCHRP <fluk.fluk.zuza@gmail.com>
2019-01-22 10:29:34 +0700

    เพิ่ม model ThecheeseLibrary ว่างโครงสร้าง MVC

---
+++ app/src/main/java/com/example/pchrp/newdashboard/MainApplication.java
@@ -0,0 +1,21 @@
+package com.example.pchrp.newdashboard;
+
+import android.app.Application;
+
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Contextor.getInstance().init(getApplicationContext());
+
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}

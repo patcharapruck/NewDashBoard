1b045f83d1ab55f64a6b9c0bb74b9ff801ed6f53
Naphatsanan <naphatsanan.boo@msu.ac.th>
2019-01-24 16:25:52 +0700

    add Dao DrinkProductItemDao

--- app/src/main/java/com/example/pchrp/newdashboard/Dao/product/DrinkBrandItemDao.java
+++ app/src/main/java/com/example/pchrp/newdashboard/Dao/product/DrinkBrandItemDao.java
@@ -1,4 +1,97 @@
 package com.example.pchrp.newdashboard.Dao.product;
 
 public class DrinkBrandItemDao {
+
+    private String recorder;
+    private String createDate;
+    private String editor;
+    private String lastUpdate;
+    private int id;
+    private String brandNameTh;
+    private String brandNameEn;
+    private String description;
+    private boolean active;
+    private boolean deleted;
+
+    public String getRecorder() {
+        return recorder;
+    }
+
+    public void setRecorder(String recorder) {
+        this.recorder = recorder;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public String getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(String lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrandNameTh() {
+        return brandNameTh;
+    }
+
+    public void setBrandNameTh(String brandNameTh) {
+        this.brandNameTh = brandNameTh;
+    }
+
+    public String getBrandNameEn() {
+        return brandNameEn;
+    }
+
+    public void setBrandNameEn(String brandNameEn) {
+        this.brandNameEn = brandNameEn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 }
+
+

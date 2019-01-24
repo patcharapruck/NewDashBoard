a338d3939be54f6dfcb14c1794b9fe431bd11151
Naphatsanan <naphatsanan.boo@msu.ac.th>
2019-01-24 16:23:02 +0700

    add Dao DrinkProductItemDao

--- app/src/main/java/com/example/pchrp/newdashboard/Dao/product/DrinkProductCatItemDao.java
+++ app/src/main/java/com/example/pchrp/newdashboard/Dao/product/DrinkProductCatItemDao.java
@@ -1,4 +1,95 @@
 package com.example.pchrp.newdashboard.Dao.product;
 
 public class DrinkProductCatItemDao {
+
+    private String recorder;
+    private String createDate;
+    private String editor;
+    private String lastUpdate;
+    private int id;
+    private String categoryNameTh;
+    private String categoryNameEn;
+    private String description;
+    private boolean active;
+    private boolean deleted;
+
+    public String getRecorder() {
+        return recorder;
+    }
+
+    public void setRecorder(String recorder) {
+        this.recorder = recorder;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public String getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(String lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryNameTh() {
+        return categoryNameTh;
+    }
+
+    public void setCategoryNameTh(String categoryNameTh) {
+        this.categoryNameTh = categoryNameTh;
+    }
+
+    public String getCategoryNameEn() {
+        return categoryNameEn;
+    }
+
+    public void setCategoryNameEn(String categoryNameEn) {
+        this.categoryNameEn = categoryNameEn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 }

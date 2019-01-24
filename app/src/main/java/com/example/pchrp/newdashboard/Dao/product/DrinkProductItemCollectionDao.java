91fb8b1118235c1a40d2d5887ab040389c898af3
Naphatsanan <naphatsanan.boo@msu.ac.th>
2019-01-24 16:51:48 +0700

    add Dao DrinkProductItemCollectionDao

--- app/src/main/java/com/example/pchrp/newdashboard/Dao/product/DrinkProductItemCollectionDao.java
+++ app/src/main/java/com/example/pchrp/newdashboard/Dao/product/DrinkProductItemCollectionDao.java
@@ -1,8 +1,14 @@
 package com.example.pchrp.newdashboard.Dao.product;
 
+import java.util.List;
+
 public class DrinkProductItemCollectionDao {
 
-
+    private List<DrinkProductItemDao> product;
+    private int withdrawUse;
+    private int purchaseAmount;
+    private int entertainAmount;
+    private int totalAll;
 
 
 

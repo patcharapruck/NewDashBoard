df89d1cd69bd01c48791cb922f27afdab6c7c8cf
Naphatsanan <naphatsanan.boo@msu.ac.th>
2019-01-24 16:52:05 +0700

    add Dao DrinkProductItemCollectionDao

--- app/src/main/java/com/example/pchrp/newdashboard/Dao/product/DrinkProductItemCollectionDao.java
+++ app/src/main/java/com/example/pchrp/newdashboard/Dao/product/DrinkProductItemCollectionDao.java
@@ -10,7 +10,43 @@ public class DrinkProductItemCollectionDao {
     private int entertainAmount;
     private int totalAll;
 
+    public List<DrinkProductItemDao> getProduct() {
+        return product;
+    }
 
+    public void setProduct(List<DrinkProductItemDao> product) {
+        this.product = product;
+    }
 
+    public int getWithdrawUse() {
+        return withdrawUse;
+    }
 
+    public void setWithdrawUse(int withdrawUse) {
+        this.withdrawUse = withdrawUse;
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public void setPurchaseAmount(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public int getEntertainAmount() {
+        return entertainAmount;
+    }
+
+    public void setEntertainAmount(int entertainAmount) {
+        this.entertainAmount = entertainAmount;
+    }
+
+    public int getTotalAll() {
+        return totalAll;
+    }
+
+    public void setTotalAll(int totalAll) {
+        this.totalAll = totalAll;
+    }
 }

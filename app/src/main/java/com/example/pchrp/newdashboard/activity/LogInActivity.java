14501c2f44e657b8b87aa22248d96f0bf7114b69
Naphatsanan <naphatsanan.boo@msu.ac.th>
2019-01-30 16:17:37 +0700

    เพิ่มหน้า login

--- app/src/main/java/com/example/pchrp/newdashboard/activity/LogInActivity.java
+++ app/src/main/java/com/example/pchrp/newdashboard/activity/LogInActivity.java
@@ -2,15 +2,40 @@ package com.example.pchrp.newdashboard.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.pchrp.newdashboard.R;
 
 public class LogInActivity extends AppCompatActivity {
-
+    EditText username,password;
+    Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        username = (EditText)findViewById(R.id.userId);
+        password = (EditText)findViewById(R.id.passId);
+        btnlogin = (Button)findViewById(R.id.btnLogin);
+
+
+
+    }
+
+    private void InitInstances(){
+
+        username.getText().toString();
+        password.getText().toString();
+
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }

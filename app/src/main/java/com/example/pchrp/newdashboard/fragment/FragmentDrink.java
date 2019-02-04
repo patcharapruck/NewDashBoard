81a1104a19a8e176615ca76d372e58c5b248c39e
PCHRP <fluk.fluk.zuza@gmail.com>
2019-02-04 07:27:00 +0700

    เลือกวันหน้าเครื่องดื่ม

--- app/src/main/java/com/example/pchrp/newdashboard/fragment/FragmentDrink.java
+++ app/src/main/java/com/example/pchrp/newdashboard/fragment/FragmentDrink.java
@@ -1,6 +1,8 @@
 package com.example.pchrp.newdashboard.fragment;
 
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -13,13 +15,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.pchrp.newdashboard.Dao.DashBoardDao;
 import com.example.pchrp.newdashboard.Dao.objectdao.ObjectItemDao;
 import com.example.pchrp.newdashboard.R;
+import com.example.pchrp.newdashboard.activity.DrinkActivity;
 import com.example.pchrp.newdashboard.activity.MainActivity;
+import com.example.pchrp.newdashboard.activity.RealIncomeActivity;
 import com.example.pchrp.newdashboard.manager.Contextor;
 import com.example.pchrp.newdashboard.manager.DashBoradManager;
+import com.example.pchrp.newdashboard.manager.http.HttpManager;
 import com.example.pchrp.newdashboard.util.SharedPrefDateManager;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
@@ -34,6 +42,15 @@ import com.github.mikephil.charting.formatter.LargeValueFormatter;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -50,6 +67,9 @@ public class FragmentDrink extends Fragment implements View.OnClickListener {
     ArrayList<Long> withdrawProduct;
     ObjectItemDao Odao;
     int size;
+    String date;
+
+    Button btncalendardrink;
 
     HorizontalBarChart barChart;
 
@@ -70,7 +90,10 @@ public class FragmentDrink extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.frag_drink, container, false);
         initInstances(rootView);
-        String date = SharedPrefDateManager.getInstance(Contextor.getInstance().getContext()).getreqDate();
+        return rootView;
+    }
+
+    private void initInstances(View rootView) {
 
         toolbar = (Toolbar) rootView.findViewById(R.id.tbDrink);
         AppCompatActivity activity = (AppCompatActivity) getActivity();
@@ -85,18 +108,9 @@ public class FragmentDrink extends Fragment implements View.OnClickListener {
                 getActivity().finish();
             }
         });
-        return rootView;
-    }
 
-    private void initInstances(View rootView) {
-        Odao = DashBoradManager.getInstance().getDao().getObject();
-        this.size = Odao.getSummaryUseProductList().size();
-
-        nameProduct = new ArrayList<>(size);
-        totalAllProduct = new ArrayList<>(size);
-        entertainProduct = new ArrayList<>(size);
-        purchaseProduct = new ArrayList<>(size);
-        withdrawProduct = new ArrayList<>(size);
+        btncalendardrink = (Button)rootView.findViewById(R.id.btncalendardrink);
+        btncalendardrink.setOnClickListener(this);
 
         tvtotalpd = (TextView) rootView.findViewById(R.id.tvtotalpd);
         tventertainpd = (TextView) rootView.findViewById(R.id.tventertainpd);
@@ -106,95 +120,11 @@ public class FragmentDrink extends Fragment implements View.OnClickListener {
 
         btndrink.setOnClickListener(this);
 
-        for (int i = 0; i < size; i++) {
-            String name = getnameProduct(i);
-            Long total = gettotalProduct(i);
-            Long entertain = getEntertain(i);
-            Long purchase = getPurchase(i);
-            Long withdraw = getWithdraw(i);
-
-            nameProduct.add(name);
-            totalAllProduct.add(total);
-            entertainProduct.add(entertain);
-            purchaseProduct.add(purchase);
-            withdrawProduct.add(withdraw);
-        }
-
-        Long sumtotal = 0L, sumentertain = 0L, sumpurchase = 0L, sumwithdraw = 0L;
-        for (int i = 0; i < size; i++) {
-            sumtotal = sumtotal + totalAllProduct.get(i);
-            sumentertain = sumentertain + entertainProduct.get(i);
-            sumpurchase = sumpurchase + purchaseProduct.get(i);
-            sumwithdraw = sumwithdraw + withdrawProduct.get(i);
-        }
-
-        tvtotalpd.setText(sumtotal.toString());
-        if (sumtotal > 0) {
-            tvtotalpd.setTextColor(Color.parseColor("#62BB47"));
-        }
-        tventertainpd.setText(sumentertain.toString());
-        if (sumentertain > 0) {
-            tventertainpd.setTextColor(Color.parseColor("#62BB47"));
-        }
-        tvpurchasepd.setText(sumpurchase.toString());
-        if (sumpurchase > 0) {
-            tvpurchasepd.setTextColor(Color.parseColor("#62BB47"));
-        }
-        tvwithdrawpd.setText(sumwithdraw.toString());
-        if (sumwithdraw > 0) {
-            tvwithdrawpd.setTextColor(Color.parseColor("#62BB47"));
-        }
-
 
         barChart = rootView.findViewById(R.id.Hbarchart);
         // Init 'View' instance(s) with rootView.findViewById here
 //        hbarChart = (BarChart) rootView.findViewById(R.id.Hbarchart);
 
-        BarDataSet Set1 = new BarDataSet(withdrawUse(), "เบิกใช้");
-        Set1.setColors(Color.parseColor("#0097A7"));
-        BarDataSet Set2 = new BarDataSet(purchaseAmount(), "ซื้อ");
-        Set2.setColors(Color.parseColor("#0277BD"));
-        BarDataSet Set3 = new BarDataSet(entertainAmount(), "Entertrain");
-        Set3.setColors(Color.parseColor("#00695C"));
-        Set1.setDrawValues(true);
-        Set2.setDrawValues(true);
-        Set3.setDrawValues(true);
-
-        BarData data = new BarData(Set1, Set2, Set3);
-        barChart.setData(data);
-        data.setValueTextSize(15f);
-
-
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(nameProduct));
-        xAxis.setCenterAxisLabels(true);
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setGranularity(1);
-        xAxis.setGranularityEnabled(true);
-        xAxis.setTextSize(10f);
-        xAxis.setLabelRotationAngle(90);
-
-        barChart.setDragEnabled(true);
-        barChart.setVisibleXRangeMaximum(2);
-
-        //set Label Center
-        float groupSpace = 0.5f;
-        float barSpace = 0f;
-        float barWidth = 0.20f;
-        data.setBarWidth(barWidth);
-        //(barwidth + barspace) * no of bars + groupspace = 1
-
-        barChart.getAxisLeft().setAxisMinimum(0);
-        barChart.groupBars(0, groupSpace, barSpace);
-        barChart.setDrawValueAboveBar(true);
-        // Hide grid lines
-        barChart.getAxisLeft().setEnabled(false);
-        barChart.getAxisRight().setEnabled(false);
-        // Hide graph description
-        barChart.getDescription().setEnabled(false);
-        // Hide graph legend
-        barChart.getLegend().setEnabled(false);
-        barChart.invalidate();
 
     }
 
@@ -252,6 +182,106 @@ public class FragmentDrink extends Fragment implements View.OnClickListener {
     @Override
     public void onStart() {
         super.onStart();
+        date = SharedPrefDateManager.getInstance(Contextor.getInstance().getContext()).getreqDate();
+        Odao = DashBoradManager.getInstance().getDao().getObject();
+        this.size = Odao.getSummaryUseProductList().size();
+        setViewDrink();
+    }
+
+    private void setViewDrink() {
+
+
+        nameProduct = new ArrayList<>(size);
+        totalAllProduct = new ArrayList<>(size);
+        entertainProduct = new ArrayList<>(size);
+        purchaseProduct = new ArrayList<>(size);
+        withdrawProduct = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            String name = getnameProduct(i);
+            Long total = gettotalProduct(i);
+            Long entertain = getEntertain(i);
+            Long purchase = getPurchase(i);
+            Long withdraw = getWithdraw(i);
+
+            nameProduct.add(name);
+            totalAllProduct.add(total);
+            entertainProduct.add(entertain);
+            purchaseProduct.add(purchase);
+            withdrawProduct.add(withdraw);
+        }
+
+        Long sumtotal = 0L, sumentertain = 0L, sumpurchase = 0L, sumwithdraw = 0L;
+        for (int i = 0; i < size; i++) {
+            sumtotal = sumtotal + totalAllProduct.get(i);
+            sumentertain = sumentertain + entertainProduct.get(i);
+            sumpurchase = sumpurchase + purchaseProduct.get(i);
+            sumwithdraw = sumwithdraw + withdrawProduct.get(i);
+        }
+
+        tvtotalpd.setText(sumtotal.toString());
+        if (sumtotal > 0) {
+            tvtotalpd.setTextColor(Color.parseColor("#62BB47"));
+        }
+        tventertainpd.setText(sumentertain.toString());
+        if (sumentertain > 0) {
+            tventertainpd.setTextColor(Color.parseColor("#62BB47"));
+        }
+        tvpurchasepd.setText(sumpurchase.toString());
+        if (sumpurchase > 0) {
+            tvpurchasepd.setTextColor(Color.parseColor("#62BB47"));
+        }
+        tvwithdrawpd.setText(sumwithdraw.toString());
+        if (sumwithdraw > 0) {
+            tvwithdrawpd.setTextColor(Color.parseColor("#62BB47"));
+        }
+
+        BarDataSet Set1 = new BarDataSet(withdrawUse(), "เบิกใช้");
+        Set1.setColors(Color.parseColor("#0097A7"));
+        BarDataSet Set2 = new BarDataSet(purchaseAmount(), "ซื้อ");
+        Set2.setColors(Color.parseColor("#0277BD"));
+        BarDataSet Set3 = new BarDataSet(entertainAmount(), "Entertrain");
+        Set3.setColors(Color.parseColor("#00695C"));
+        Set1.setDrawValues(true);
+        Set2.setDrawValues(true);
+        Set3.setDrawValues(true);
+
+        BarData data = new BarData(Set1, Set2, Set3);
+        barChart.setData(data);
+        data.setValueTextSize(15f);
+
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(nameProduct));
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setTextSize(10f);
+        xAxis.setLabelRotationAngle(90);
+
+        barChart.setDragEnabled(true);
+        barChart.setVisibleXRangeMaximum(2);
+
+        //set Label Center
+        float groupSpace = 0.5f;
+        float barSpace = 0f;
+        float barWidth = 0.20f;
+        data.setBarWidth(barWidth);
+        //(barwidth + barspace) * no of bars + groupspace = 1
+
+        barChart.getAxisLeft().setAxisMinimum(0);
+        barChart.groupBars(0, groupSpace, barSpace);
+        barChart.setDrawValueAboveBar(true);
+        // Hide grid lines
+        barChart.getAxisLeft().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
+        // Hide graph description
+        barChart.getDescription().setEnabled(false);
+        // Hide graph legend
+        barChart.getLegend().setEnabled(false);
+        barChart.invalidate();
+
     }
 
     @Override
@@ -268,9 +298,40 @@ public class FragmentDrink extends Fragment implements View.OnClickListener {
         // Save Instance State here
     }
 
-    /*
-     * Restore Instance State Here
-     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        reqAPI(SharedPrefDateManager.getInstance(Contextor.getInstance().getContext()).getreqDate());
+    }
+
+    private void reqAPI(String date) {
+        final Context mcontext = Contextor.getInstance().getContext();
+        String nn = "{\"property\":[],\"criteria\":{\"sql-obj-command\":\"( tb_sales_shift.open_date >= '"+date+" 00:00:00' AND tb_sales_shift.open_date <= '"+date+" 23:59:59')\",\"summary-date\":\"*\"},\"orderBy\":{\"InvoiceDocument-id\":\"desc\"},\"pagination\":{}}";
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"),nn);
+        Call<DashBoardDao> call = HttpManager.getInstance().getService().loadAPI(requestBody);
+        call.enqueue(new Callback<DashBoardDao>() {
+
+            @Override
+            public void onResponse(Call<DashBoardDao> call, Response<DashBoardDao> response) {
+                if(response.isSuccessful()){
+                    DashBoardDao dao = response.body();
+                    DashBoradManager.getInstance().setDao(dao);
+                }else {
+                    Toast.makeText(mcontext,"เกิดข้อผิดพลาด",Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<DashBoardDao> call, Throwable t) {
+                Toast.makeText(mcontext,"ไม่สามารถเชื่อมต่อกับข้อมูลได้",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -296,11 +357,6 @@ public class FragmentDrink extends Fragment implements View.OnClickListener {
         for (int i = 0; i < size; i++) {
             barBnk2.add(new BarEntry(i, purchaseProduct.get(i)));
         }
-//        barBnk2.add(new BarEntry(1, 20f));
-//        barBnk2.add(new BarEntry(2, 50f));
-//        barBnk2.add(new BarEntry(3, 20f));
-//        barBnk2.add(new BarEntry(4, 79f));
-//        barBnk2.add(new BarEntry(5, 35f));
         return barBnk2;
     }
 
@@ -311,12 +367,6 @@ public class FragmentDrink extends Fragment implements View.OnClickListener {
         for (int i = 0; i < size; i++) {
             barBnk2.add(new BarEntry(i, entertainProduct.get(i)));
         }
-
-//        barBnk2.add(new BarEntry(1, 29f));
-//        barBnk2.add(new BarEntry(2, 2f));
-//        barBnk2.add(new BarEntry(3, 4f));
-//        barBnk2.add(new BarEntry(4, 0f));
-//        barBnk2.add(new BarEntry(5, 0f));
         return barBnk2;
     }
 
@@ -329,5 +379,45 @@ public class FragmentDrink extends Fragment implements View.OnClickListener {
                     .addToBackStack(null)
                     .commit();
         }
+
+        if(v == btncalendardrink){
+            DatePickerDialog dialog = new DatePickerDialog(getContext(),new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    month++;
+                    String mm = ""+month;
+                    String dd = ""+dayOfMonth;
+
+                    if (month<10){
+                        mm = "0"+month;
+                    }
+                    if (dayOfMonth < 10){
+                        dd = "0"+dayOfMonth;
+                    }
+                    String datecalendat;
+                    datecalendat = year+ "/" + mm + "/" +dd;
+
+                    SharedPrefDateManager.getInstance(Contextor.getInstance().getContext())
+                            .saveDatereq(datecalendat);
+
+                    SharedPrefDateManager.getInstance(Contextor.getInstance().getContext())
+                            .saveDateCalendar(dayOfMonth,month,year);
+
+
+
+                    getActivity().recreate();
+
+                }
+            },SharedPrefDateManager.getInstance(Contextor.getInstance().getContext()).getYear()
+                    ,SharedPrefDateManager.getInstance(Contextor.getInstance().getContext()).getMonth()
+                    ,SharedPrefDateManager.getInstance(Contextor.getInstance().getContext()).getDateofMonth());
+
+            Calendar c = Calendar.getInstance(Locale.ENGLISH);
+            c.add(Calendar.DATE,-1);
+            Date date = c.getTime();
+            dialog.getDatePicker().setMaxDate(date.getTime());
+            dialog.show();
+        }
+
     }
 }
